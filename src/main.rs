a2d0631a493f2dc0ccb317e30aa29f864803b212
dedefer <ddf1998@gmail.com>
2021-05-21 02:34:35 +0300

    no static shit 3)

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,10 @@ use crate::pic_bot::PicBot;
 async fn main() -> Result<(), Box<dyn Error>> {
     openssl_probe::init_ssl_cert_env_vars();
 
-    let config = Settings::read()?;
-    log::set_max_level(config.log_level);
+    let Settings { bot_token, api_token, log_level } = Settings::read()?;
+    log::set_max_level(log_level);
 
-    let bot = PicBot::new(config.bot_token, config.api_token);
+    let bot = PicBot::new(bot_token, api_token);
 
     bot.start().await;
 
